d06d4ef7e8478c4449fb1ea5c7644788b89332d8
Leon <ali11586298@gmail.com>
2024-10-16 22:59:53 -0400

    small fix

--- src/app/itineraries/ItinerariesList.tsx
+++ src/app/itineraries/ItinerariesList.tsx
@@ -1,4 +1,5 @@
 'use client';
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
 import { Card, CardHeader, CardTitle, CardDescription, CardContent } from '@/components/ui/card';
