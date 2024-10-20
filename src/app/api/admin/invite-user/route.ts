c4f02894a850e7f88ef5f6998a0d32bb71538032
Leon <ali11586298@gmail.com>
2024-10-19 21:26:31 -0400

    invite user

---
+++ src/app/api/admin/invite-user/route.ts
@@ -0,0 +1,22 @@
+import { supabaseAdmin } from '@/utils/supabase/admin';
+import { NextResponse } from 'next/server';
+
+export async function POST(request: Request) {
+  const { email, password } = await request.json();
+  const redirectOrigin = request.headers.get('origin');
+  const redirectTo = `${redirectOrigin}/auth/callback`;
+
+  const { error } = await supabaseAdmin.auth.signUp({
+    email,
+    password,
+    options: {
+      emailRedirectTo: redirectTo,
+    },
+  });
+
+  if (error) {
+    return NextResponse.json({ error: error.message }, { status: 400 });
+  }
+
+  return NextResponse.json({ success: true });
+}
